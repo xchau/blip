d6dc6f1a2c367fee1b98f3636daa06b1467f0f34
Minh Chau <xiu.chau@gmail.com>
2017-03-13 11:34:38 -0700

    Set up basic routing

---
+++ app/containers/Registration.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Button, Text, View } from 'react-native';
+
+export default class Registration extends Component {
+  constructor(props) {
+    super(props);
+
+    this.navigate = this.navigate.bind(this);
+    this.goBack = this.goBack.bind(this);
+  }
+
+  navigate(route) {
+    this.props.navigator.push({name: route});
+  }
+
+  goBack(route) {
+    this.props.navigator.pop({name: route});
+  }
+
+  render() {
+    return <View>
+      <Text>Registration Page</Text>
+      <Button
+        onPress={() => this.navigate('login')}
+        title="Go To Login"
+      />
+      <Button
+        color="lightcoral"
+        onPress={() => this.goBack('login')}
+        title="Go Back"
+      />
+    </View>
+  }
+};
--- index.ios.js
+++ index.ios.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  StyleSheet,
   Text,
+  Navigator,
   View
 } from 'react-native';
+import Login from './app/containers/Login';
+import Registration from './app/containers/Registration';
 
 export default class Capstone extends Component {
+  constructor() {
+    super();
+
+    this.renderScene = this.renderScene.bind(this);
+  }
+
+  renderScene(route, navigator) {
+    switch (route.name) {
+      case 'login': {
+        return <Login
+          navigator={navigator}
+        />
+        break;
+      }
+      case 'registration': {
+        return <Registration
+          navigator={navigator}
+        />
+        break;
+      }
+      default: {
+        return <View>
+          <Text>ERROR</Text>
+        </View>
+      }
+    }
+  }
+
   render() {
     return (
-      <View>
-
-      </View>
+      <Navigator
+        initialRoute={{name: 'login'}}
+        renderScene={this.renderScene}
+      />
     );
   }
 }
