8d0459a9206f995888893b81909bf99fd095de7b
Minh Chau <xiu.chau@gmail.com>
2017-03-13 11:04:06 -0700

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class Capstone extends Component {
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('Capstone', () => Capstone);
